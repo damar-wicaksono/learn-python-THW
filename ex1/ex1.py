911448b4b361e08ee5199d4d8220302e884c3ea4
Damar Wicaksono <damar.wicaksono@gmail.com>
2014-11-17 11:52:16 +0100

    Add exercise 1 base file
    Simple multiple `print` statements exercise

---
+++ ex1/ex1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# Learn Python the Hard Way, 3rd Edition
+# Exercise 1: A Good First Program
+#
+print("Hello world!")
+print("Hello again.")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
