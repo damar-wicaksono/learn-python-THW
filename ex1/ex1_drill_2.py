ebf83dcb1e6a01214a5dd9642b3b803c19ca0285
Damar Wicaksono <damar.wicaksono@gmail.com>
2014-11-17 11:53:16 +0100

    Add ex1 study drills 2
    Commented most of the `print` statements, printing only one line

---
+++ ex1/ex1_drill_2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Learn Python the Hard Way, 3rd Edition
+# Exercise 1: A Good First Program
+# Study Drills:
+#   1. Make your script print another line.
+#   2. Make your script print only one of the lines.
+
+print("Hello world!")
+#print("Hello again.")
+#print("I like typing this.")
+#print("This is fun.")
+#print('Yay! Printing.')
+#print("I'd much rather you 'not'.")
+#print('I "said" do not touch this.')
+#print("Don't say I didn't 'warn' you!")
