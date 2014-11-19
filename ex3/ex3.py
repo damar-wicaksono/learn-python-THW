6275979e2bb345082a9210e9245e408b2f6d79a0
Damar Wicaksono <damar.wicaksono@gmail.com>
2014-11-19 14:54:50 +0100

    Add Exercise 3 main python file
    On some numbers and math

---
+++ ex3/ex3.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Learn Python the Hard Way, 3rd Edition
+# Adapted for python3
+# Exercise 3: Numbers and Math
+#
+
+print("I will now count my chickens:")
+
+print("Hens", 25 + 30 / 6) #30
+print("Roosters", 100 - 25 * 3 % 4) #1
+
+print("Now I will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 // 4 + 6) # Here `//` is an integer division
+
+print("Is it true that 3 + 2 < 5 - 7?") # False
+
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2) # True
+print("Is it greater or equal?", 5 >= -2) # True
+print("Is it less or equal?", 5 <= -2) # False
