e36b45e59dd2e60d4bd26e3c47ab0f65f627c632
Damar Wicaksono <damar.wicaksono@gmail.com>
2014-11-19 14:55:40 +0100

    Add Ex3 Study Drills scripts
    1. Commenting each lines
    2. Custom made python basic calculation ability
    3. Using float number

---
+++ ex3/ex3_drills_3.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Learn Python the Hard Way, 3rd Edition
+# Adapted for python3
+# Exercise 3: Numbers and Math
+# Study Drills 3
+#
+
+# Print statement the purpose of calculation
+print("I will now count my chickens:")
+
+# Print statement calculating the number of hens
+print("Hens", 25.0 + 30.0 / 6.0) #30
+# Print statement calculating the number of roosters
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0) #1
+
+# A remark now calculating the eggs
+print("Now I will count the eggs:")
+
+# Print statement calculating the number of roosters
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+# Another remark
+print("Is it true that 3 + 2 < 5 - 7?") # False
+
+# Print statement of result from comparison operation
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+# Print statement from above comparison operation, left hand side (addition)
+print("What is 3 + 2?", 3.0 + 2.0)
+# Print statement from above comparison operation, right hand side (subtraction)
+print("What is 5 - 7?", 5.0 - 7.0)
+
+# Print statement, simple remark
+print("Oh, that's why it's False.")
+
+# Print statement, simple remark
+print("How about some more.")
+
+# another print statement on the result of comparison operation, greater-than
+print("Is it greater?", 5.0 > -2.0) # True
+# another statement on the result of comparison operation, greater-or-equal-to
+print("Is it greater or equal?", 5.0 >= -2.0) # True
+# another statement on the result of comparison operation, less-or-equal-to
+print("Is it less or equal?", 5.0 <= -2.0) # False
